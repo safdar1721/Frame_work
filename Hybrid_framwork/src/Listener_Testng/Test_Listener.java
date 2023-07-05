389ded99ff70246724253d3ec1a4fa372650198e
HP <HP@DESKTOP-MSNJAQA>
2023-07-05 18:16:12 +0530

    b03

--- Hybrid_framwork/src/Listener_Testng/Test_Listener.java
+++ Hybrid_framwork/src/Listener_Testng/Test_Listener.java
@@ -9,12 +9,12 @@ public class Test_Listener implements ITestListener {
 	public void onTestStart(ITestResult result) {
 		System.out.println("tc is started");
 	}
-
+//tc is pass
 	@Override
 	public void onTestSuccess(ITestResult result) {
 		System.out.println("tc is passed");
 	}
-
+//tcis fail
 	@Override
 	public void onTestFailure(ITestResult result) {
 		System.out.println("tc is failed");
